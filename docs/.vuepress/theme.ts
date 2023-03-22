2f67277fafd27645ed26c91cffc824e621d9d765
Stephen-wzw <wzw15292257101@163.com>
2023-03-22 12:19:30 +0800

    style: icon

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -16,7 +16,7 @@ export default hopeTheme({
   navbar: Navbar,
   sidebar: Sidebar,
 
-  iconAssets: '//at.alicdn.com/t/c/font_3926422_2v6urabrwiu.css',
+  iconAssets: '//at.alicdn.com/t/c/font_3926422_nwhvguyhfk.css',
   iconPrefix: 'iconfont icon-',
 
   repo: 'https://github.com/dribble-njr/blog',

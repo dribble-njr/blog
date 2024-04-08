cd07cfe31853a1099ae74f9c7f5d50635d9cfa8f
dribble-njr <wzw15292257101@163.com>
2024-04-08 17:07:59 +0800

    feat: pragmatic-paranoid

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -28,7 +28,7 @@ export default hopeTheme(
       end: ['Language', 'Repo', 'Outlook', 'Search']
     },
 
-    iconAssets: '//at.alicdn.com/t/c/font_3926422_vcv6pgsuzgq.css',
+    iconAssets: '//at.alicdn.com/t/c/font_3926422_ran1rifa7f.css',
     iconPrefix: 'iconfont icon-',
 
     repo: 'https://github.com/dribble-njr/blog',
